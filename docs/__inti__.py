d5be5fb5694c9ffe90cfce85838b38fb57605196
Alfredo <alfredo.delafuente.b@gmail.com>
2016-10-23 11:12:23 -0500

    Uplaod Files

--- docs/__inti__.py
+++ docs/__inti__.py
@@ -9,15 +9,15 @@ target = cv2.imread("/home/alfredo/PycharmProjects/PolyPic/examples/IM_04.jpg")
 height, width = target.shape[:2]
 
 #Run Hill Climbing Algorithm
-#hc = hill_climb.HillClimbing( 100001 , target , 60 , 2)
-#hc.run("SolutionHC_Error_100000_337.268905473.txt")
+#hc = hill_climb.HillClimbing( 100001 , target , 60 , 3)
+#hc.run("SolutionHC_Error_100000_434.853631841.txt")
 
 #Run Simulated Annealing Algorithm
-sa = sim_annealing.Simulated_Annealing(100001,target,60,3)
-sa.run()
+#sa = sim_annealing.Simulated_Annealing(100001,target,60,3)
+#sa.run("SolutionSA_Error_100000_586.249253731.txt")
 
-#ind = individual.IndividualGen("SolutionHC_Error_100000_332.787960199.txt")
-#ind.write("MONA_LISA_APROX_2.jpg")
+#ind = individual.IndividualGen("SolutionHC_Error_100000_429.987412935.txt")
+#ind.write("MONA_LISA_APROX_3.jpg")
 
 #Run Genetic Algorithm
 #ga = gen_algo.GeneticAlgorithm
