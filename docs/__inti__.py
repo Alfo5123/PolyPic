ecc8480e24340ba487db8de90135fa1b33e3777a
Alfredo <alfredo.delafuente.b@gmail.com>
2016-11-12 10:02:22 -0500

    Upload Files

--- docs/__inti__.py
+++ docs/__inti__.py
@@ -6,8 +6,16 @@ import refine
 import cv2
 
 # Load the image
-target = cv2.imread("/home/alfredo/PolyPic/examples/BIRDS_TRAIN.jpg")
-height, width = target.shape[:2]
+#workdir = "/home/alfredo/PolyPic/examples/"
+#image_name = "BIRDS.jpg"
+#target = cv2.imread(workdir+image_name)
+#height, width = target.shape[:2]
+
+# Resize Image for speeding up training
+#scale = 0.5
+#train = cv2.resize( target, (0,0), fx=scale, fy=scale)
+#train_name = image_name[:-4] + "_TRAIN.jpg"
+#cv2.imwrite(workdir + train_name,train)
 
 #Run Hill Climbing Algorithm
 #hc = hill_climb.HillClimbing( 100001 , target , 200 , 4)
@@ -20,13 +28,12 @@ height, width = target.shape[:2]
 # Refine
 #source = cv2.imread("/home/alfredo/PolyPic/examples/STARRY_NIGHT_APROX.jpg")
 #target = cv2.imread("/home/alfredo/PolyPic/examples/STARRY_NIGHT.jpg")
-
 #ex = refine.Refine( 10000 , 50 , 1 , source , target )
 #ex.run()
 
-ind = individual.IndividualGen("SolutionHC_Error_100000_430.692232906.txt")
-ind.write("BIRDS_APROX.jpg")
-#ind.encode("STARRY_NIGHT_1_749.862888889.txt")
+# Generate Individual
+#ind = individual.IndividualGen("SolutionHC_Error_100000_430.692232906.txt")
+#ind.write("BIRDS_APROX.jpg")
 
 #Run Genetic Algorithm
 #ga = gen_algo.GeneticAlgorithm(100, 100, 100, height, width , target , 3)
